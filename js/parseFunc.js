aef9dec5e9e54983a789f53045a1b9ff0f65489e
Felipe Kenzo <Kenzo12152@gmail.com>
2021-03-15 19:02:43 -0300

    Updates

---
+++ js/parseFunc.js
@@ -0,0 +1,19 @@
+// parse func
+const tableParseJson = tableHtmlStr => {
+  const trRe = /<tr>.*?<\/tr>/gs;
+  const trs = tableHtmlStr.match(trRe); // console.log(trs)
+  // trs.forEach(v => console.log(v))
+
+  const tdRe = /<td>(.*?)<\/td>/gs;
+  const tableArray = trs.map((tr, i) => {
+    tds = [...tr.matchAll(tdRe)];
+    const trtds = tds.map(td => td[1]); // console.log(i, trtds, '')
+    // console.log(`${i}: ${trtds}\n`)
+
+    return trtds;
+  }); // parse
+
+  const tableParsed = JSON.stringify(tableArray);
+  console.log(tableParsed);
+  return tableParsed;
+};
