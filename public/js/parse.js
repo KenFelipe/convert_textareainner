02cbf5f48a38ad0eafdf9abd50d233432268c996
Felipe Kenzo <Kenzo12152@gmail.com>
2021-03-15 13:36:52 -0300

    init

---
+++ public/js/parse.js
@@ -0,0 +1,6 @@
+const parseTable = () => {
+  const inputField = document.getElementById('input-field');
+  const outputField = document.getElementById('output-field');
+  const inputValue = inputField.value;
+  console.log(inputValue); // regex
+};
